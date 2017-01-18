cf3634c9e60770b861e3efb25659060841715b9d
Ryan Kline <trk0002@uah.edu>
2017-01-18 17:03:36 -0600

    Made the hello world program actually use PHP

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <html>
-<body>
-Hello World!
-</body>
+   <head>
+      <title>Hello World!</title>      
+   </head>
+   
+   <body>
+      
+      <?php
+         echo "<h1>Hello, PHP!</h1>";
+      ?>
+   
+   </body>
 </html>
