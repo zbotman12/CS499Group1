c9f44897f07d9921353a2946614fe23bec63d7c5
jeanmalmonte <jc0051@uah.edu>
2017-02-18 20:37:24 -0600

    update changepass.php

--- changepass.php
+++ changepass.php
@@ -11,6 +11,9 @@
     <body>
         <form id="changepass" action="changepasstest.php" method="post">
             <input type='hidden' name='submitted' id='submitted' value='1'/>
+            
+            <label for ='currentPass'>Current Password: </label>
+            <input type='password' name='currentPass' id='currentPass' maxlength='25'/><br/>
 
             <label for='newPass' >New Password:</label>
             <input type='password' name='newPass' id='newPass' maxlength="25"/> <br/>
@@ -18,7 +21,10 @@
             <label for='updatedPass'>Confirm Password:</label>
             <input type='password' name='updatedPass' id='updatedPass' maxlength="25"/><br/>
             
-            <input type='submit' name='Submit' value='Submit' />
+            <input type='submit' name='Submit' value='Submit' /> <br/>
+
+            <a href="./logintest.php"> Login</a><br/>
+            <a href="./changepass.php"> Change Your Password</a><br/>
         </form>
     </body>    
 </html>
--- changepasstest.php
+++ changepasstest.php
@@ -13,12 +13,12 @@
         $DB_PW       = '';           //Database access password
         $DB_NAME     = 'ParagonMLS'; //Name of database to be accessed
         $TABLE_NAME  = 'Agents';     //Name of the table to be accessed
-
+        
         //Obtain Agent ID and Username. Hardcoded for now.
         //session_start();
         //$AGENT_USERNAME = $_SESSION['name'];
-        
-        $AGENT_USERNAME = 'jbonds';    //Username for this agent. Must obtain this from session.
+         
+        $username = 'jbonds';          //Username for this agent. Must obtain this from session.
         //$AGENT_ID = 1;               //Must obtain this from current session
 
         // Connect to DB
@@ -26,18 +26,27 @@
         
         //Check connection to database.
         if ($conn->connect_error) {
-            echo "Connection failed: " . $conn->connect_error;
+            //echo "Connection failed: " . $conn->connect_error;
         } else {
             //echo "Connection successful<br/>";
         } 
 
+        //Quarantine Zone
+        $password    = sanitizer($_POST['currentPass']);
+        $newPass     = sanitizer($_POST['newPass']);
+        $confirmPass = sanitizer($_POST['updatedPass']);
+
+        //Build Query
         $query = "SELECT * FROM " . $TABLE_NAME . " ";
-        $query .= "WHERE user_login = \"" . $AGENT_USERNAME . "\";";
+        $query .= "WHERE user_login = \"" . $username . "\" ";
+        $query .= "AND password = \"" . $password . "\";";
 
         //Query database for username
         $result = $conn->query($query);
+       
+        //Check if user exists
+        $userExists = false;
 
-        // Check if user exists
         if ($result) {
             if ($result->num_rows == 1) {
                 $userExists = true;
@@ -49,36 +58,27 @@
         }
 
         if ($userExists == true) {
-            //User exists. Go ahead and change password
-           	
-            //Sanitize the input
-            $newPass = sanitizer($_POST['newPass']);
-            $confirmPass = sanitizer($_POST['updatedPass']);
-
-            //Check if passwords match
+            //Check if new passwords match
             if (strcmp($newPass, $confirmPass) != 0) {
-                echo "Could not change your password. Passwords do not match!";
-                //$conn->close();
+                echo "Could not change your password. Passwords entered do not match!<br/>";
             } else {
-                $query = "UPDATE " . $TABLE_NAME . " SET password=" . "'" . $confirmPass . "'" . " WHERE user_login=" . "'" . $AGENT_USERNAME . "'";
+                $query = "UPDATE " . $TABLE_NAME . " SET password=" . "'" . $confirmPass . "'" . " WHERE user_login=" . "'" . $username . "'";
                 
                 //Talk to database and update this agents table entry
                 if (mysqli_query($conn, $query)) {
-                	echo "Password succesfully changed! <br/>";	
-                	//$conn->close();
+                    echo "Password successfully changed! <br/>"; 
+                    //$conn->close();
                 } else {
-            		echo "Could not change your password. SQL Error.<br/>";
-            		echo $conn->error;
-            		//$conn->close();
-            		//exit();
-            	}
+                    echo "Could not change your password. SQL Error.<br/>";
+                    echo $conn->error;
+                    //$conn->close();
+                    //exit();
+                }
             }
         } else {
             //That means user doesn't exist and this will be serious error
             //We must ensure that we get the username for agent correctly
-
-            //$conn->close(); //Close connection to database if user doesn't exist
-            echo "Could not change your password. Agent does not exist in database. Contact your system administrator.<br/>";
+            echo "Could not change your password. Agent does not exist in database. Please input your password or contact your system administrator.<br/>";
         }
 
         //Close database connection
