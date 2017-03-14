59120023c9a6a81c67fa28ad0b45c1ebc1465b5b
CS499Group1 <jc0051@uah.edu>
2017-03-14 14:17:25 -0500

    file permissions

--- dbconnect.php
+++ dbconnect.php
@@ -4,11 +4,12 @@
     *NOTE* you must call $conn->close(); when you finish your database operations.
 -->
 <?php
-        $DB_LOCATION = 'localhost';  //Server URL
-        $DB_USERNAME = 'root';       //Database access username
-        $DB_PW       = '';           //Database access password
-        $DB_NAME     = 'ParagonMLS'; //Name of database to be accessed
-        $TABLE_NAME  = 'Agents';     //Name of the table to be accessed
+        $config = parse_ini_file('/var/www/config.ini');
+        $DB_LOCATION = $config['dblocation'];  //Server URL
+        $DB_USERNAME = $config['username'];    //Database access username
+        $DB_PW       = $config['password'];    //Database access password
+        $DB_NAME     = $config['dbname'];      //Name of database to be accessed
+        $TABLE_NAME  = 'Agents';               //Name of the table to be accessed
         
         // Connect to DB
         $conn = new mysqli($DB_LOCATION, $DB_USERNAME, $DB_PW, $DB_NAME);
