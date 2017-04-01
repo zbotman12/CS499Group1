618dd83316b51964286256e513f5293d154932be
xKungFuChickenx <clp0022@uah.edu>
2017-04-01 13:09:57 -0500

    Add files via upload

--- Showing Schedule/newShowingDataHandle.php
+++ Showing Schedule/newShowingDataHandle.php
@@ -1,14 +1,19 @@
 <?php
+// ISSUE: Is_vacant ($occupy) is not display correctly.
 
-// ISSUE: Check for duplicate entries in database
 header('location: ./../showings.php?MLS=' . $_POST['MLS']);
-//include './../sessioncheck.php"; -- Possible remove this. talk to Michael
 include "../DBTransactor/DBTransactorFactory.php";
 handleShowingData();
-// ADD FILE NAME FOR RYANS LISTING FILE into header to redirect back to listings
+
+/*
+ * func: handleShowingData
+ * param: N/A
+ * desc: Handles formatting data submitted from newShowingDisplay.php
+ * 		 form and creating a new showing based on that information. 
+ * 		 Generates a feedback page for each showing.
+ */
 function handleShowingData(){
-	$feedback= null;
-    $showings = null;
+	// Try to establish connections to database
 	try {
 	  $feedback=DBTransactorFactory::build("Showing_Feedback");
       $showings=DBTransactorFactory::build("Showings");
@@ -17,6 +22,8 @@ function handleShowingData(){
 	} catch (Exception $e) {
       echo $e->getMessage();
     }
+    // Intialize variables to hold form data from newShowingDisplay.php
+    // and set them to empty strings
 	$startHour=$startMin=$startTime=$endHour=$endMin=$endTime=$date=$occupy=
 	$fname=$lname=$code=$MLS="";
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -27,15 +34,14 @@ function handleShowingData(){
 		$endMin = test_input($_POST["endMin"]);
 		$endTime = test_input($_POST["endTime"]);
 		$date = test_input($_POST["date"]);
-		$occupy = $_POST["occupy"];
+		$occupy = intval($_POST["occupy"]);
 		$fname = test_input($_POST["fname"]);
 		$lname = test_input($_POST["lname"]);
 		$code = test_input($_POST["code"]);
 		$MLS= intval($_POST['MLS']);
 		$complete_info=test_input($_POST["SAgent"]);
-	
 	}
-	
+	// Lines 42-62: Used to format and parse agent info.
 	$parsed_info=parseShowingAgentData($complete_info);
 	$SA_first_name= $parsed_info[1];
 	$SA_last_name= $parsed_info[0];
@@ -48,8 +54,6 @@ function handleShowingData(){
 	//var_dump($confirmed_company);
 	//$result_information = ob_get_clean();
 	//error_log($result_information,0);
-	
-	//$select_company=$confirmed_company->fetch_array();
 	$select_company=array_pop($confirmed_company);
 	$temp_array3= array("agent_id");
 	$temp_array4= array("first_name"=>$SA_first_name, "last_name"=>$SA_last_name, "Agencies_agency_id"=>$select_company["agency_id"]);
@@ -59,11 +63,10 @@ function handleShowingData(){
 	$SA_id=$select_SA_id["agent_id"];
 	//error_log("agent id ".$SA_id,0);
 	
-
-	// Lines 37,38 are just used to set an hour time to 0-23 for formatting
+	// Lines 66-67 are just used to set an hour time to 0-23 for formatting
 	$startHour = fixTimeFormat($startTime, $startHour);
 	$endHour = fixTimeFormat($endTime, $endHour);
-	// lines 41,42 set up variables for str to date conversions
+	// lines 69-70 set up variables for str to date conversions
 	$finalStartFormat = $date." ".$startHour.$startMin.":00";
 	$finalEndFormat = $date." ".$endHour.$endMin.":00";
     
@@ -74,61 +77,42 @@ function handleShowingData(){
                     "customer_first_name"   => $fname,
                     "customer_last_name"    => $lname,
                     "lockbox_code"          => $code,
-                    "showing_agent_id"      => $SA_id);
-    
-    //Lines 69-72 are used to check if the showing is already scheduled
-    //$test_exist_var= array("showing_id");
-   // $test_exist_already=$showings->select($test_exist_var, $array);
+                    "showing_agent_id"    => $SA_id);
     
     try {
-      $showings->insert($array);
-      
+      $showings->insert($array); // add new showing
       $return_name= array("showing_id");
       $showing_info= $showings->select($return_name, $array);
       
-      
+   	  // Create feedback info for the new showing
 	  $feedback_array = Array("Showings_showing_id"=>$showing_info[1]["showing_id"],
 	  						  "customer_interest_level"=>2,
 	  						  "showing_agent_experience_level"=>2,
-	  						  "customer_price_option"=>2,
+	  						  "customer_price_opinion"=>2,
 	  						  "additional_notes"=>"");
 	  $feedback->insert($feedback_array);
-	  
-	
-      //var_dump($showings);
       header('location: ./../showings.php?MLS=' . $_POST['MLS']);
      } catch (Exception $e) {
       echo $e->getMessage() . "<br/>";  
     }
-    //}
-   // else
-   /* {
-    	echo "
-            <script type=\"text/javascript\">
-            alert(\"Showing has already been scheduled\");
-            </script>
-        ";
-    }*/
-    mysql_close($showings);
-    mysql_close($feedback_array);
-    
 }
 
+// Parse out string containing agent info into array
 function parseShowingAgentData($full_info)
 {	//error_log($full_info, 0);
-	$parsed_array = array();
+	$parsed_array = array(); // Lines 120-122: Parse out last name
 	$pos=strrpos($full_info, ",");
 	$tempStr=substr($full_info, 0, $pos);
 	array_push($parsed_array, $tempStr);
 	//error_log("$pos= ".$pos, 0);
-	$pos2=strrpos($full_info, " of ");
+	$pos2=strrpos($full_info, " of "); // Lines 125-127: Parse out first name
 	$tempStr=substr($full_info, $pos+2, $pos2-$pos-2);
 	array_push($parsed_array, $tempStr);
 	//error_log("$pos2= ".$pos2, 0);
-	$tempStr=substr($full_info, $pos2+4);
+	$tempStr=substr($full_info, $pos2+4); // Lines 129-130: Parse out Company
 	array_push($parsed_array, $tempStr);
 	
-	return $parsed_array;
+	return $parsed_array; 
 }
 
 // Sets 1-12 hour to 0-23 hour format
