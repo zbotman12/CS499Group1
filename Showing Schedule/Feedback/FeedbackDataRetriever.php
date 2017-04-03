9ada3e08fc1eb2a284cec577f42b20bd938d9af5
xKungFuChickenx <clp0022@uah.edu>
2017-04-03 01:38:10 -0500

    Add files via upload

--- Showing Schedule/Feedback/FeedbackDataRetriever.php
+++ Showing Schedule/Feedback/FeedbackDataRetriever.php
@@ -1,5 +1,5 @@
 <?php
-	include "DBTransactor/DBTransactorFactory.php";
+	include "../DBTransactor/DBTransactorFactory.php";
 	
 	
 	function get_FeedbackInfo()
@@ -11,51 +11,50 @@
 	// Pull data from Showing Feedback
 	$tempArray= array("customer_interest_level", "showing_agent_experience_level","customer_price_opinion","additional_notes");
 	$cond= array("Showings_showing_id"=>$_GET['showing_id']);
-	if ($result = $conn->select($tempArray, $cond)) {
-		while ($row = $result->fetch_array()){
-			$customer_interest = $row["customer_interest_level"];
-			$showing_agent_exp = $row["showing_agent_experience_level"];
-			$customer_price = $row["customer_price_opinion"];
-			$addtional_notes = $row["additional_notes"];
-		}
-	}
+	$result = $conn2->select($tempArray, $cond);
+	ob_start();
+	var_dump($result);
+	$result_information = ob_get_clean();
+	error_log($result_information,0);
+	$row = array_pop($result);
+	$customer_interest = $row["customer_interest_level"];
+	$showing_agent_exp = $row["showing_agent_experience_level"];
+	$customer_price = $row["customer_price_opinion"];
+	$additional_notes = $row["additional_notes"];
+		
 	
 	//Pull data from Showing
 	$tempArray= array("is_house_vacant", "customer_first_name", "customer_last_name", "lockbox_code", 
 	"showing_agent_id");
 	$cond= array("showing_id"=>$_GET['showing_id']);
-	if ($result = $conn2->select($tempArray, $cond)) {
-		while ($row = $result->fetch_array()){
-			$vacant = $row["is_house_vacant"];
-			$customer_first_name = $row["customer_first_name"];
-			$customer_last_name = $row["customer_last_name"];
-			$code = $row["lockbox_code"];
-			$showing_agent_id=$row["showing_agent_id"];
-		}
-	}
-	
+	$result = $conn->select($tempArray, $cond);
+	$row = array_pop($result);
+	$vacant = $row["is_house_vacant"];
+	$customer_first_name = $row["customer_first_name"];
+	$customer_last_name = $row["customer_last_name"];
+	$code = $row["lockbox_code"];
+	$showing_agent_id=$row["showing_agent_id"];
+		
 	//Pull data from Agents
 	$tempArray= array("first_name", "last_name", "email", "phone_number");
 	$cond= array("agent_id"=>$showing_agent_id);
-	if ($result = $conn3->select($tempArray, $cond)) {
-		while ($row = $result->fetch_array()){
-			$first_name = $row["first_name"];
-			$last_name = $row["last_name"];
-			$email = $row["email"];
-			$phone= $row["phone_number"];
-		}
-	}
+	$result = $conn3->select($tempArray, $cond);
+	$row = array_pop($result);
+	$first_name = $row["first_name"];
+	$last_name = $row["last_name"];
+	$email = $row["email"];
+	$phone= $row["phone_number"];
 	
 
-	
 	$dataArray=array($customer_interest, $showing_agent_exp, $customer_price, $additional_notes,
 			$vacant, $customer_first_name, $customer_last_name, $code, $showing_agent_id,
 			$first_name, $last_name, $email, $phone);
 	
-	mysql_close($conn);
-	mysql_close($conn2);
-	mysql_close($conn3);
-	
+	/*ob_start();
+	var_dump($dataArray);
+	$result_information = ob_get_clean();
+	error_log($result_information,0);
+	*/
 	return $dataArray;
 	}
 	
