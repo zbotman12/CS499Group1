fcd4fa63860ef5e13351e357b6901fc2ca5ebaa6
Ryan Kline <trk0002@uah.edu>
2017-04-10 13:28:46 -0500

    added a clear filters button to the listings page

--- listings.php
+++ listings.php
@@ -128,6 +128,7 @@
 				<label for="zipCode">ZIP Code</label>
 				<input type="text" name="zipCode" value="<?php echo $_GET["zipCode"]; ?>"/>
 				<button type="submit" class="btn btn-primary">Search</button>
+				<button type="reset" value="Reset" class="btn btn-primary">Clear Filters</button>
 				<input type="hidden" value="1" name="page"/>
 			</div>
 		</form>
